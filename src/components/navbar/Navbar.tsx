c654f7b5bea02c9697524f9f3b389f9715bf38a8
Sami Anki <sami.anki1316@gmail.com>
2023-02-16 01:34:02 +0100

    feat(navbar): Add navbar + prettier + reset css

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import "./Navbar.scss";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <span className="text">fiverr</span>
+          <span className="dot">.</span>
+        </div>
+
+        <div className="links">
+          <span>Fiverr Business</span>
+          <span>Explore</span>
+          <span>English</span>
+          <span>Sign in</span>
+          <span>Become a Seller</span>
+          <button>Join</button>
+        </div>
+      </div>
+      <hr />
+
+      <div className="menu">
+        <span>test</span>
+        <span>test2</span>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
